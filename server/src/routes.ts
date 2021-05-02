c4d6c0b59a3b21599c7b2819c8f0921da11ed37d
Márcio Corrêa <msc@outlook.com.br>
2021-05-02 01:02:52 -0300

    User routes created

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,9 +1,21 @@
-import { Router } from 'express'
+import { Router } from 'express';
 import IndexController from '@controllers/IndexController'
+import UserController from '@controllers/UserController';
 
 const router = Router()
-const indexController = new IndexController()
+const indexController = new IndexController();
+const userController = new UserController();
 
-router.get('/ping', indexController.ping)
+router.get('/ping', indexController.ping);
 
-export default router
+router.route('/user/:id')
+    .get(userController.getUser)
+    .put(userController.updateUser)
+    .patch(userController.updateUser)
+    .delete(userController.deleteUser);
+
+router.route('/user')
+    .post(userController.addUser)
+    .get(userController.getUsers);
+
+export default router;
