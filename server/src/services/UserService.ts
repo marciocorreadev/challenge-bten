a533e39c4c7a4171e9b79e93a86415f064e597fa
Márcio Corrêa <msc@outlook.com.br>
2021-05-02 12:07:42 -0300

    Add passowrd with hash and email

--- server/src/services/UserService.ts
+++ server/src/services/UserService.ts
@@ -1,5 +1,6 @@
 import User from '@models/User';
-import { DeleteResult, UpdateResult, getConnection } from 'typeorm';
+import * as bcrypt from 'bcrypt';
+import { DeleteResult, getConnection, UpdateResult } from 'typeorm';
 
 import UserRepository from '../repositories/UsersRepository';
 
@@ -7,7 +8,7 @@ class UserService {
   static async getUsers(skip: number, limit: number, keyword: string) {
     const usersRepository = getConnection(process.env.NODE_ENV).getCustomRepository(UserRepository);
     return await usersRepository.createQueryBuilder('user').take(limit).skip(skip)
-      .where("user.height || user.homeTeam || user.name like '%' || :keyword || '%'", { keyword })
+      .where("user.email || user.height || user.homeTeam || user.name like '%' || :keyword || '%'", { keyword })
       .getManyAndCount() as [User[], number];
   }
 
@@ -16,9 +17,15 @@ class UserService {
     return await usersRepository.findOne(userId) as User;
   }
 
+  static async findOne(email: string) {
+    const usersRepository = getConnection(process.env.NODE_ENV).getCustomRepository(UserRepository);
+    return await usersRepository.findOne({ email }) as User;
+  }
+
   static async addUser(user: User) {
     const usersRepository = getConnection(process.env.NODE_ENV).getCustomRepository(UserRepository);
-    const createUser: User = usersRepository.create(user);
+    const password: string = await bcrypt.hash(user.password, 10);
+    const createUser: User = usersRepository.create({ ...user, password });
     return await usersRepository.save(createUser) as User;
   }
 
