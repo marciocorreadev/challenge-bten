cc714f3824d1716b24b2766ee592a6d41f6aa36d
Márcio Corrêa <msc@outlook.com.br>
2021-05-02 01:02:04 -0300

    User validate created

--- server/src/validators/index.ts
+++ server/src/validators/index.ts
@@ -1,1 +1,13 @@
 import * as yup from 'yup'
+import User from '@models/User'
+
+function userValidate(user: User) {
+    return yup.object().shape({
+        name: yup.string().required(),
+        homeTeam: yup.string().required(),
+        age: yup.date().required(),
+        height: yup.string().required()
+    }).validate(user, { abortEarly: false })
+}
+
+export { userValidate }
