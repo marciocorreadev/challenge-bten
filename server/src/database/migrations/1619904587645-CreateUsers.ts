216f785651e558404e2cc3d49b23c223166f2f5a
Márcio Corrêa <msc@outlook.com.br>
2021-05-02 12:11:05 -0300

    App error adjustment

---
+++ server/src/database/migrations/1619904587645-CreateUsers.ts
@@ -0,0 +1,56 @@
+import { MigrationInterface, QueryRunner, Table } from 'typeorm';
+
+export class CreateUsers1619904587645 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: 'users',
+        columns: [
+          {
+            name: 'id',
+            type: 'uuid',
+            isPrimary: true,
+          },
+          {
+            name: 'name',
+            type: 'varchar',
+          },
+          {
+            name: 'email',
+            type: 'varchar',
+          },
+          {
+            name: 'password',
+            type: 'varchar',
+          },
+          {
+            name: 'home_team',
+            type: 'varchar',
+          },
+          {
+            name: 'age',
+            type: 'timestamp',
+          },
+          {
+            name: 'height',
+            type: 'double precision',
+          },
+          {
+            name: 'created_at',
+            type: 'timestamp',
+            default: 'now()',
+          },
+          {
+            name: 'update_at',
+            type: 'timestamp',
+            default: 'now()',
+          },
+        ],
+      }),
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable('users');
+  }
+}
--- server/src/errors/AppErro.ts
+++ server/src/errors/AppErro.ts
@@ -1,9 +1,13 @@
+import { Response } from 'express';
+
 export default class AppError {
     public readonly message: string;
     public readonly statusCode: number;
 
-    constructor(message: string, statusCode = 400) {
+    constructor(resposne:Response, message: string, statusCode = 400) {
       this.message = message;
       this.statusCode = JSON.stringify(message).includes('ValidationError') ? 400 : statusCode;
+
+      resposne.status(this.statusCode).json({ error: this.message });
     }
 }
